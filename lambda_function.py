64b5026c0fb37ab8df988996ded42ef71cf2d716
AsMoh <asiamohamoud2014@gmail.com>
2022-11-30 21:03:47 +0400

    Updated

--- lambda_function.py
+++ lambda_function.py
@@ -43,26 +43,26 @@ data={ 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_p
 target_size= 150
 
 
-classes = [
-    'dino',
-    'dragon'
-]
-
 def predict(url):
     img=download_image(url)
     img=prepare_image(img, (target_size,target_size))
     x=np.array(img,dtype='float32')
-    X = np.array([x])
+    X=np.array([x])
     X=X/255.0
+    X
     
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_index)
-
+    if preds[0][0] <= 0.5:
+        label='dino'
+    else:
+        label='dragon'
+        
     float_predictions = preds[0].tolist()
+    label=list([label])
 
-    return dict(zip(classes, float_predictions))
-
+    return dict(zip(label, float_predictions))
 
 def lambda_handler(event, context):
     url = event['url']
